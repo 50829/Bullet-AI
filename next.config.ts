7fdffe9204df2c3297b55e33e34a8d4853f881b9
50829 <hydronmezo@gmail.com>
2025-08-24 22:06:24 +0800

    vite base path added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  base: process.env.VITE_BASE_PATH || "/react-test1",
 };
 
 export default nextConfig;
